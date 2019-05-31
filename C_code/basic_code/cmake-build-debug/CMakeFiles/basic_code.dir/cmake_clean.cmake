0df02123a6c86a866328e120a4500d6e46e000c2
shiguang5200 <809094812@qq.com>
2019-05-31 17:45:13 +0800

    XS basic code

---
+++ C_code/basic_code/cmake-build-debug/CMakeFiles/basic_code.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/basic_code.dir/main.c.obj"
+  "basic_code.pdb"
+  "basic_code.exe"
+  "basic_code.exe.manifest"
+  "libbasic_code.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/basic_code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
